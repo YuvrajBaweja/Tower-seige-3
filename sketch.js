6183ebf247aa807dd7e25d764f4e1064e315fa5d
YuvrajBaweja <81843637+YuvrajBaweja@users.noreply.github.com>
2022-01-09 17:39:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,128 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint
+var boy , boyimage
+var tree , treeimage
+var stone
+var block,block2,block3,block4,block5,block6,block7
+var ground
+var hexagon
+var polygonimg
+var score 
+score = 0
+
+function preload()
+{
+    hexagon =loadImage("polygon.png")
+
+}
+
+
+function setup() {
+	createCanvas(800, 700);
+    stone=Bodies.circle(50,200,20)
+
+	engine = Engine.create();
+	world = engine.world;
+    block1=new Block(240,425,30,30)
+    block2=new Block(270,425,30,30)
+    block3=new Block(300,425,30,30)
+    block4=new Block(330,425,30,30)
+    block5=new Block(360,425,30,30)
+    block6=new Block(390,425,30,30)
+    block7=new Block(420,425,30,30)
+    block8=new Block(240,400,30,30)
+    block9=new Block(270,400,30,30)
+    block10=new Block(300,400,30,30)
+    block11=new Block(330,400,30,30)
+    block12=new Block(360,400,30,30)
+    block13=new Block(260,375,30,30)
+    block14=new Block(290,375,30,30)
+    block15=new Block(320,375,30,30)
+    block16=new Block(280,350,30,30)
+    polygon=Bodies.circle(50,300,20)
+    World.add(world,polygon)
+    slingshot=new SlingShot(this.polygon,{x:50,y:300})
+	//Create the Bodies Here.
+    //boy = createSprite(170,600)
+    //boy.addImage(boyimage)
+    //boy.scale=0.125
+	//tree = createSprite(500,400)
+	//tree.addImage(treeimage)
+	//tree.scale=0.400
+	///stone.scale = 0.200
+	//stone = new Stone(170,600,30,1)
+    
+    ground = new Ground(320,450,250,30)
+    
+	Engine.run(engine);
+	//ground = new Ground(10,690,300,30)
+   
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  //stone.display()
+  fill("skyblue")
+  block1.display()
+  block1.score()
+  block2.display()
+  block2.score()
+  block3.display()
+  block3.score()
+  block4.display()
+  block4.score()
+  block5.display()
+  block5.score()
+  block6.display()
+  block6.score()
+  block7.display()
+  block7.score()
+  fill("grey")
+  block8.display()
+  block8.score()
+  block9.display()
+  block9.score()
+  block10.display()
+  block10.score()
+  block11.display()
+  block11.score()
+  block12.display()
+  block12.score()
+  fill("red")
+  block13.display()
+  block14.score()
+  block14.display()
+  block14.score()
+  block15.display()
+  block15.score()
+  fill("yellow")
+  block16.display()
+  image(hexagon,polygon.position.x,polygon.position.y,40,40)
+  text("SCORE:"+score,750,40)
+
+
+  ground.display()
+  slingshot.display()
+
+  drawSprites();
+ 
+  
+}
+function mouseDragged(){
+    Matter.Body.setPosition(this.polygon, {x: mouseX , y: mouseY});
+  }
+  
+  function mouseReleased(){
+    slingshot.fly();
+    //gameState = "launched";
+}
+
+
+
